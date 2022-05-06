8b51da44cdb711ce0da722bd2e39284bd4a3eec0
Lucas Chae <hi@lucaschae.com>
2022-05-06 14:57:34 +0900

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Lucas Chae on 5/6/22.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
